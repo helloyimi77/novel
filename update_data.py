f75fe33b3a69270ba8dd94769942df48da77bd17
kazuya yoshizawa <helloyimi@gmail.com>
2026-08-25 11:41:30 +0900

    Add files via upload

--- update_data.py
+++ update_data.py
@@ -361,6 +361,7 @@ def render_book(book, kasasagi, kakuyomu, naro_cumulative, naro_history):
     shortTitle: {js_string(book["shortTitle"])},
     status: {js_string(book["status"])},
     genre: {js_string(book.get("genre", ""))},
+    order: {book.get("order", 99)},
     episodes: {len(kakuyomu["episodes"])},
     tags: [{", ".join(js_string(t) for t in tags)}],
     mood: {js_string(book["mood"])},
